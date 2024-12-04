944ba41a8d13e003f3a909589a70104a4c40c4c9
Amar Srivastava <amars@mx>
2024-12-04 16:12:42 +0530

    configured registration of the user

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,34 +1,50 @@
-import CommonForm from '@/components/common/form'
-import { registerFormControls } from '@/config'
-import { registerUser } from '@/store/auth-slice'
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import {Link, useNavigate} from 'react-router-dom'
+import CommonForm from "@/components/common/form";
+import { registerFormControls } from "@/config";
+import { registerUser } from "@/store/auth-slice";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 const Register = () => {
-    const initialState  = {
-        userName : "" , 
-        email  : "" , 
-        password : "" 
-    }
-    const [ formData , setFormData] = useState(initialState )
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-console.log(formData)
-    function onSubmit (event) {
-      event.preventDefault()
-      dispatch(registerUser(formData)).then((data)=> {console.log(data);
-      })
-    }
+  const initialState = {
+    userName: "",
+    email: "",
+    password: "",
+  };
+  const [formData, setFormData] = useState(initialState);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  console.log(formData);
+  function onSubmit(event) {
+    event.preventDefault();
+    dispatch(registerUser(formData)).then((data) => {
+      if (data?.payload?.success) navigate("/auth/login");
+    });
+  }
   return (
-    <div className='mx-auto w-full max-w-md space-y-6 '>
-        <div className='text-center '>
-            <h1 className='text-3xl font-bold text-foreground tracking-tight '>Registration for new User </h1>
-        <p className='mt-2'>Already have an account ? <Link className='font-medium  ml-2 text-primary hover:underline' to = '/auth/login'>Login here </Link></p>
-        
-        </div>
-        <CommonForm onSubmit={onSubmit} formControls={registerFormControls} formData={formData} setFormData={setFormData} buttonText='create New account'/>
+    <div className="mx-auto w-full max-w-md space-y-6 ">
+      <div className="text-center ">
+        <h1 className="text-3xl font-bold text-foreground tracking-tight ">
+          Registration for new User{" "}
+        </h1>
+        <p className="mt-2">
+          Already have an account ?{" "}
+          <Link
+            className="font-medium  ml-2 text-primary hover:underline"
+            to="/auth/login"
+          >
+            Login here{" "}
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        onSubmit={onSubmit}
+        formControls={registerFormControls}
+        formData={formData}
+        setFormData={setFormData}
+        buttonText="create New account"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
